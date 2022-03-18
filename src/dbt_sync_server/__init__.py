7ec9a9bd81d63a3195d4973d424520fa361902f2
z3z1ma <butler.alex2010@gmail.com>
2022-03-18 13:11:04 -0700

    make rpc injection optional

--- src/dbt_sync_server/__init__.py
+++ src/dbt_sync_server/__init__.py
@@ -3,6 +3,7 @@ A super lightweight abstraction of the dbt rpc which serves out synchronous requ
 and affords more customization. Should also afford us the ability to swap the RPC
 with another solution like `dbt.lib` which dbt server may implicitly use.
 """
+from email.policy import default
 from typing import Dict
 import multiprocessing
 import subprocess
@@ -68,21 +69,23 @@ def cli():
 @click.option("--port", type=click.INT, default=8581)
 @click.option("--rpc-port", type=click.INT, default=8580)
 @click.option("--project-dir", type=click.Path(exists=True, file_okay=False, dir_okay=True), default="./")
-def serve(port: int = 8581, rpc_port: int = 8580, project_dir: str = "./"):
+@click.option("--inject-rpc", type=click.BOOL, default=False)
+def serve(port: int = 8581, rpc_port: int = 8580, project_dir: str = "./", inject_rpc: bool = False):
     STATE["server"] = DbtClient(port=rpc_port)
-    rpc_server = multiprocessing.Process(target=run_rpc, args=(rpc_port, project_dir), daemon=True)
-    rpc_server.start()
-    time.sleep(2.5)
-    if not rpc_server.is_alive():
-        exit_code = rpc_server.exitcode
-        rpc_server.close()
-        if exit_code == 0:
-            print("RPC failed to initialize, exit code {} most likely indicates a process is already running on port {} or the project directory provided [{}] is not a valid dbt project.".format(exit_code, rpc_port, project_dir))
-        elif exit_code == 1:
-            print("RPC failed to initialize, exit code {} most likely indicates the dbt project is invalid or has an error.".format(exit_code))
-        else:
-            print("RPC failed to initialize, exit code {} with unknown root cause.".format(exit_code))
-        exit(1)
+    if inject_rpc:
+        rpc_server = multiprocessing.Process(target=run_rpc, args=(rpc_port, project_dir), daemon=True)
+        rpc_server.start()
+        time.sleep(2.5)
+        if not rpc_server.is_alive():
+            exit_code = rpc_server.exitcode
+            rpc_server.close()
+            if exit_code == 0:
+                print("RPC failed to initialize, exit code {} most likely indicates a process is already running on port {} or the project directory provided [{}] is not a valid dbt project.".format(exit_code, rpc_port, project_dir))
+            elif exit_code == 1:
+                print("RPC failed to initialize, exit code {} most likely indicates the dbt project is invalid or has an error.".format(exit_code))
+            else:
+                print("RPC failed to initialize, exit code {} with unknown root cause.".format(exit_code))
+            exit(1)
     try:
         app.run("localhost", port)
     finally:
